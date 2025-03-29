8d455e90561f51936e0e409ab7785eb69a830de3
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-29 11:52:44 -0300

    ✨ feat: Adicionado interface de usuarios

---
+++ Frontend/src/@types/usuario.ts
@@ -0,0 +1,12 @@
+export interface UsuarioProps{
+    id               : number,
+    name             : string,
+    email            : string, 
+    serviceType      : string,
+    phone            : string,
+    address          : string,
+    city             : string,
+    neighborhood     : string,
+    postalCode       : string,
+    isEmailVerified  : boolean,
+}
