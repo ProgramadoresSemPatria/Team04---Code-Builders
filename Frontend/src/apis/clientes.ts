402b02d4fe0345dc894d58286270afbb1f2fd8b9
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-29 11:54:34 -0300

    ✨ feat: Adicionado api de cliente

--- Frontend/src/apis/clientes.ts
+++ Frontend/src/apis/clientes.ts
@@ -2,16 +2,16 @@ import  {ClientesProps} from '../@types/clientes';
 
 
 const data = [
-	{ name: "João Silva", age: 29, city: "São Paulo",project : 1 },
-	{ name: "Maria Oliveira", age: 34, city: "Rio de Janeiro",project : 1 },
-	{ name: "Carlos Souza", age: 23, city: "Belo Horizonte",project : 1 },
-	{ name: "Ana Costa", age: 25, city: "Curitiba",project : 1 },
-	{ name: "Pedro Santos", age: 31, city: "Salvador",project : 1 },
-	{ name: "Lucia Martins", age: 27, city: "Fortaleza",project : 1 },
-	{ name: "Fernanda Lima", age: 35, city: "Recife",project : 1 },
-	{ name: "Lucas Pereira", age: 30, city: "Porto Alegre",project : 1 },
-	{ name: "Camila Rocha", age: 22, city: "Goiânia",project : 1 },
-	{ name: "Bruna Alves", age: 28, city: "Manaus",project : 1 },
+	{ id:1,name: "João Silva", age: 29, city: "São Paulo",project : 1 },
+	{ id:2,name: "Maria Oliveira", age: 34, city: "Rio de Janeiro",project : 1 },
+	{ id:3,name: "Carlos Souza", age: 23, city: "Belo Horizonte",project : 1 },
+	{ id:4,name: "Ana Costa", age: 25, city: "Curitiba",project : 1 },
+	{ id:5,name: "Pedro Santos", age: 31, city: "Salvador",project : 1 },
+	{ id:6,name: "Lucia Martins", age: 27, city: "Fortaleza",project : 1 },
+	{ id:7,name: "Fernanda Lima", age: 35, city: "Recife",project : 1 },
+	{ id:8,name: "Lucas Pereira", age: 30, city: "Porto Alegre",project : 1 },
+	{ id:9,name: "Camila Rocha", age: 22, city: "Goiânia",project : 1 },
+	{ id:10,name: "Bruna Alves", age: 28, city: "Manaus",project : 1 },
 ];
 
 export async function GetAllClients(): Promise<ClientesProps[] | []> {
@@ -34,33 +34,17 @@ export async function GetAllClients(): Promise<ClientesProps[] | []> {
 
 
 
-// export async function GetDadosCliente(idcliente:number): Promise<ClientesProps> {
+export async function GetDadosCliente(idcliente:number): Promise<ClientesProps> {
 
-//     const token = await Getcookieserver()
-//     const idempresa = await Getidempresaserver()
-    
-//     try {
+    try {
 
-
-//         const retorno = await api.get(`/clientes/listar/${idempresa}`, {         
-//                 headers: {
-//                 Authorization: `Bearer ${token}`,
-//             },
-//         });
-
-
-        
-//         const filter = retorno.data.filter((item : ClientesProps) => {
-//             return Number(item.idcliente) === Number(idcliente); 
-//         })
-
-        
-//         return filter[0] as ClientesProps; 
+		const retorno = data.find((item) => item.id === idcliente);
+   
+        return retorno as ClientesProps; 
   
 
-//     }catch(err){   
-
-//         console.log(err)
-//         return {} as ClientesProps
-//     }
-// }
+    }catch(err){   
+        console.log(err)
+        return {} as ClientesProps
+    }
+}
