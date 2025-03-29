b095936dee031b41d831e2ec76fe9298a273b410
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-28 23:22:58 -0300

    ✨ feat: Interface de clientes

---
+++ Frontend/src/@types/clientes.ts
@@ -0,0 +1,15 @@
+
+
+export interface ClientesProps{
+    id?:number,
+    userId?: number,
+    age: number,
+    name : string,
+    email?: string ,
+    phone         ?:string,
+    address       ?:string,
+    city          :string,
+    neighborhood  ?:string,
+    postalCode    ?:string,
+    project       :number
+}
