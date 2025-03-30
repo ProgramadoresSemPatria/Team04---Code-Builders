7f051c2975811de6f1bf22c381d09f1e6b0304dd
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-30 09:45:59 -0300

    🚚 chore: Alterado propriedades

--- Frontend/src/@types/clientes.ts
+++ Frontend/src/@types/clientes.ts
@@ -2,14 +2,13 @@
 
 export interface ClientesProps{
     id:number,
-    userId?: number,
-    age: number,
+    userId: number,
     name : string,
-    email?: string ,
-    phone         ?:string,
-    address       ?:string,
+    email: string,
+    phone       :string,
+    address     ?:string,
     city          :string,
-    neighborhood  ?:string,
-    postalCode    ?:string,
-    project       :number
+    neighborhood?:string,
+    postalCode :string,
+
 }
