c85760488ebda3520ac456c2b099dea846351d71
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-31 01:10:36 -0300

    🚚 chore: Atualizado vite.config.js

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,
-    port: 5173
+    port: 5173,
+    host: '0.0.0.0', // Permite conexões externas
+    strictPort: true, // Garante que a porta 5173 será usada
+    allowedHosts: ['team04-code-builders.onrender.com'] // Permite o domínio do Render
   }
 })
