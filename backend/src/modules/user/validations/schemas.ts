0f1dc0a49e08ab644a64e0912255a363b6f3594c
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-31 20:38:33 -0300

    🚚 chore: Alterado schema

--- backend/src/modules/user/validations/schemas.ts
+++ backend/src/modules/user/validations/schemas.ts
@@ -1,14 +1,15 @@
 import { z } from 'zod';
 
+
 export const updateUserSchema = z.object({
   name: z.string().min(2).max(100).optional(),
   email: z.string().email().optional(),
   serviceType: z.string().max(50).optional(),
-  password: z.string().min(6).optional(),
-  phone: z.string().optional(),
-  address: z.string().max(200).optional(),
-  city: z.string().max(100).optional(),
-  neighborhood: z.string().max(100).optional(),
-  postalCode: z.string().optional(),
+  password: z.string().optional().nullable(),
+  phone: z.string().nullable().optional(),
+  address: z.string().max(200).nullable().optional(),
+  city: z.string().max(100).nullable().optional(),
+  neighborhood: z.string().max(100).nullable().optional(),
+  postalCode: z.string().nullable().optional(),
   isPaymentDone: z.boolean().optional(),
 });
