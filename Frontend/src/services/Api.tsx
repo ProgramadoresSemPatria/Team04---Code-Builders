aa0ff82e0df04e0565ee024fa70e7f3a9f51ef8a
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-31 01:23:04 -0300

    ✨ feat: Adicionado url da api

--- Frontend/src/services/Api.tsx
+++ Frontend/src/services/Api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : '/api',    
+    baseURL: 'https://apis-slyz.onrender.com/api',    
 });
 
 
