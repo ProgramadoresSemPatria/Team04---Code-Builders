d1b0a327383b2900467ca775d6a7c5ee7c1970bf
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-31 12:46:38 -0300

    🚚 cleanup: Removido espaços.

--- Frontend/src/@types/projetos.ts
+++ Frontend/src/@types/projetos.ts
@@ -1,10 +1,10 @@
 
 
 export interface ProjetosProps {
-    id: number;
-    userId: number;
-    name: string;
-    clientId : number;
-    status  : string;
-    price  : number;
+    id:number;
+    userId:number;
+    name:string;
+    clientId:number;
+    status:string;
+    price:number;
 }
