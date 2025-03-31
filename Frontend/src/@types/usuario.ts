3559d555261f043d43f17839ed68eeac8168c818
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-31 12:47:50 -0300

    🚚 chore: Comentado código

--- Frontend/src/@types/usuario.ts
+++ Frontend/src/@types/usuario.ts
@@ -1,15 +1,15 @@
-export interface UsuarioProps{
-    id               : number,
-    name             : string,
-    email            : string, 
-    serviceType      : string,
-    phone            : string,
-    address          : string,
-    city             : string,
-    neighborhood     : string,
-    postalCode       : string,
-    isEmailVerified  : boolean,
-}
+// export interface UsuarioProps{
+//     id               : number,
+//     name             : string,
+//     email            : string, 
+//     serviceType      : string,
+//     phone            : string,
+//     address          : string,
+//     city             : string,
+//     neighborhood     : string,
+//     postalCode       : string,
+//     isEmailVerified  : boolean,
+// }
 
 
 export  interface AuthResponse {
